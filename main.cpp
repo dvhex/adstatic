f63dc09b64fb662e282768d65e4278370a95d6c2
vladimir <dabarov@gmail.com>
2013-03-14 21:46:37 +0700

    Добавлена лицензия

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 /*
  * main.cpp
  *
- *  Created on: 14.03.2013
- *      Author: vladimir
+ * Copyright (c) 2013 Vladimir Dabarov
+ *
+ * This file is part of adstatic.
+ * 
+ * Adstatic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Adstatic is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Adstatic.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 /*
